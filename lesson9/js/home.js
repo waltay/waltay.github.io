2556ae7744819d27a24e0683dc62058ca354fb91
waltay <walterht@byui.edu>
2021-06-16 16:15:31 -0600

    week9.home.1

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -8,54 +8,68 @@ fetch(requestURL)
 
     const townInfo = jsonObject['towns'];
 
-    for(i = 0; i < townInfo.length; i++){
+    for(i = 0; i<3; i++){
 
-        if(townInfo[i].name == 'Fish Haven' || townInfo[i].name == 'Preston' || townInfo[i].name == 'Soda Springs'){
-
-            let card = document.createElement('section');
-            let info = document.createElement('div');
-            let h2 = document.createElement('h2');
-            let h5 = document.createElement('h5');
-            let p = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let image = document.createElement('img');
-
-            info.setAttribute('class', 'infoContainer');
-            h2.textContent = townInfo[i].name;
-            h5.textContent = townInfo[i].motto;
-            p.textContent = 'Year Founded: ' + townInfo[i].yearFounded;
-            p2.textContent = 'Population: ' + townInfo[i].currentPopulation;
-            p3.textContent = 'Annual Rainfall: ' + townInfo[i].averageRainfall;
-
-            info.appendChild(h2);
-            info.appendChild(h5);
-            info.appendChild(p);
-            info.appendChild(p2);
-            info.appendChild(p3);
-            
-            if(townInfo[i].name == 'Fish Haven'){
-                image.setAttribute('src', 'images/rsz_fishhaven.jpg');
-                image.setAttribute('alt', 'Fish Haven');
-                image.setAttribute('class', 'right');
-            }
-            else if(townInfo[i].name == 'Preston'){
-                image.setAttribute('src', 'images/rsz_preston.jpg');
-                image.setAttribute('alt', 'Preston');
-                card.setAttribute('class', 'left');
-            }
-            else if(townInfo[i].name == 'Soda Springs'){
-                image.setAttribute('src', 'images/rsz_sodasprings.jpg');
-                image.setAttribute('alt', 'Soda Springs');
-                image.setAttribute('class', 'right');
-            }
-
-            card.appendChild(info);
-            card.appendChild(image);
-
-            document.querySelector('div.townCards').appendChild(card);
+        var tName;
+        if(i == 0){
+            tName = "Preston"
+        }
+        else if(i == 1){
+            tName = "Soda Springs"
+        }
+        else if(i ==2){
+            tName = "Fish Haven"
         }
 
+        for(i = 0; i < townInfo.length; i++){
+
+            if(townInfo[i].name == tName){
+
+                let card = document.createElement('section');
+                let info = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let h5 = document.createElement('h5');
+                let p = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let image = document.createElement('img');
+
+                info.setAttribute('class', 'infoContainer');
+                h2.textContent = townInfo[i].name;
+                h5.textContent = townInfo[i].motto;
+                p.textContent = 'Year Founded: ' + townInfo[i].yearFounded;
+                p2.textContent = 'Population: ' + townInfo[i].currentPopulation;
+                p3.textContent = 'Annual Rainfall: ' + townInfo[i].averageRainfall;
+
+                info.appendChild(h2);
+                info.appendChild(h5);
+                info.appendChild(p);
+                info.appendChild(p2);
+                info.appendChild(p3);
+                
+                if(townInfo[i].name == 'Fish Haven'){
+                    image.setAttribute('src', 'images/rsz_fishhaven.jpg');
+                    image.setAttribute('alt', 'Fish Haven');
+                    image.setAttribute('class', 'right');
+                }
+                else if(townInfo[i].name == 'Preston'){
+                    image.setAttribute('src', 'images/rsz_preston.jpg');
+                    image.setAttribute('alt', 'Preston');
+                    card.setAttribute('class', 'right');
+                }
+                else if(townInfo[i].name == 'Soda Springs'){
+                    image.setAttribute('src', 'images/rsz_sodasprings.jpg');
+                    image.setAttribute('alt', 'Soda Springs');
+                    image.setAttribute('class', 'left');
+                }
+
+                card.appendChild(info);
+                card.appendChild(image);
+
+                document.querySelector('div.townCards').appendChild(card);
+            }
+
+        }
     }
   });
 
@@ -135,3 +149,5 @@ document.getElementById("current-date").innerHTML = currentDate;
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+document.getElementById("copyYear").innerHTML = year;
